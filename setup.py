2dd583e532294cf6cc66d8101b7b73eb54ddef12
Olivier DOLE <odole@wyplay.com>
2016-11-09 18:15:00 +0100

    Do not create extra log file when using xbuild (#87898).
    * When running xbuild, as long as "clean" is not called, logs are
     appended to a log file in PORT_LOGDIR. Then portage may parse this file
     during qmerge phase which may takes an endless time when the log file
     has grown up to 100MB or more. That's why from this commit, we tell
     xbuild not to generate this log file, anyway there is always
     temp/build.log file to find the same info.

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ def find_packages(dir):
 
 setup(
     name = "xportage",
-    version = "2.3.5",
+    version = "2.3.6",
     description = "Xportage tools for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",
